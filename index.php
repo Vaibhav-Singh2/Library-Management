c94221e584505a09f8305124ed043a21ce8e37ee
Vaibhav Singh <mathura.vs@gmail.com>
2024-01-22 20:23:58 +0530

    updated

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Online Library</title>
+</head>
+
+<body>
+
+    <?php include("base-layout.html") ?>
+
+    <div class="right">
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="1book.webp" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="2book.jpeg" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="3book.jpg" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="1book.webp" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="2book.jpeg" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="3book.jpg" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="1book.webp" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="2book.jpeg" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+        <div class="card">
+            <div class="book-title">Enter Book Title</div>
+            <img class="book-image" src="3book.jpg" alt="Book Cover">
+            <div class="description">
+                Insert a brief description or summary of the book here. Include key themes, characters, and any
+                other
+                relevant information to capture the reader's interest.
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
+const currentTheme = localStorage.getItem("userTheme");
+if (currentTheme == "dark-theme") {
+    const root = document.documentElement;
+    root.style.setProperty('--bg-color-1', '#352F44');
+    root.style.setProperty('--bg-color-2', '#5C5470');
+    root.style.setProperty('--bg-color-3', '#B9B4C7');
+    root.style.setProperty('--bg-color-4', '#FAF0E6');
+    const icon = document.querySelector(".theme-btn img");
+    icon.src = "./assets/dark-theme.svg";
+}
 
-    const currentTheme = localStorage.getItem("userTheme");
-    if (currentTheme == "dark-theme") {
-        const root = document.documentElement;
-        root.style.setProperty('--bg-color-1', '#352F44');
+// If user try to change theme
+const changeTheme = document.querySelector(".theme-btn");
+changeTheme.addEventListener("click", () => {
+    const userTheme = localStorage.getItem("userTheme");
+if (userTheme == "dark-theme") {
+    const root = document.documentElement;
+    root.style.setProperty('--bg-color-1', '#9681EB');
+    root.style.setProperty('--bg-color-2', '#45CFDD');
+    root.style.setProperty('--bg-color-3', '#A7EDE7');
+    root.style.setProperty('--bg-color-4', '#0000EE');
+    const icon = document.querySelector(".theme-btn img");
+    icon.src = "./assets/light-theme.svg";
+    localStorage.setItem("userTheme", "light-theme");
+}
+else {
+    const root = document.documentElement;
+    root.style.setProperty('--bg-color-1', '#352F44');
         root.style.setProperty('--bg-color-2', '#5C5470');
         root.style.setProperty('--bg-color-3', '#B9B4C7');
         root.style.setProperty('--bg-color-4', '#FAF0E6');
         const icon = document.querySelector(".theme-btn img");
         icon.src = "./assets/dark-theme.svg";
+        localStorage.setItem("userTheme", "dark-theme");
     }
+}
+);
 
-    // If user try to change theme
-    const changeTheme = document.querySelector(".theme-btn");
-    changeTheme.addEventListener("click", () => {
-        const userTheme = localStorage.getItem("userTheme");
-        if (userTheme == "dark-theme") {
-            const root = document.documentElement;
-            root.style.setProperty('--bg-color-1', '#9681EB');
-            root.style.setProperty('--bg-color-2', '#45CFDD');
-            root.style.setProperty('--bg-color-3', '#A7EDE7');
-            root.style.setProperty('--bg-color-4', '#0000EE');
-            const icon = document.querySelector(".theme-btn img");
-            icon.src = "./assets/light-theme.svg";
-            localStorage.setItem("userTheme", "light-theme");
-        }
-        else {
-            const root = document.documentElement;
-            root.style.setProperty('--bg-color-1', '#352F44');
-            root.style.setProperty('--bg-color-2', '#5C5470');
-            root.style.setProperty('--bg-color-3', '#B9B4C7');
-            root.style.setProperty('--bg-color-4', '#FAF0E6');
-            const icon = document.querySelector(".theme-btn img");
-            icon.src = "./assets/dark-theme.svg";
-            localStorage.setItem("userTheme", "dark-theme");
-        }
-    }
-    );
-
-
+document.addEventListener('DOMContentLoaded', () => {
     // For Hamburger functunality
     let hamburger = document.querySelector("#hamburger");
     hamburger.addEventListener("click", () => {
